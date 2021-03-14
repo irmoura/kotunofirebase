8fc6ef4d099e8ed0145a2482dff2c1dcb09b12aa
Ismael Ribeiro <coragethedogcowardle@gmail.com>
2021-03-14 11:40:00 -0300

    Alterações diversas

--- src/components/CardSender.jsx
+++ src/components/CardSender.jsx
@@ -5,57 +5,80 @@ import './PlayerCards.css'
 function CardSender() {
 
     function handleSubmit(arg) {
-        var card = {
+        var playerCard = {
             number: arg.target.innerText,
             color: arg.target.style.backgroundColor
         }
-        MessagingService.updateCard(card)
+        var tableCard = {
+            number: document.querySelector('.card').innerHTML,
+            cor: document.querySelector('.card').style.backgroundColor
+        }
+        var cor = playerCard.color
+        if (cor === 'red' || cor === 'green' || cor === 'blue' || cor === 'yellow') {
+            if (playerCard.color === tableCard.cor || playerCard.number === tableCard.number) {
+                // alert(`Jogada permitida`)
+                MessagingService.updateCard(playerCard)
+            } else {
+                console.log(`Jogada não permitida`)
+            }
+        }
+        //🚫🔄
     }
 
     return <div className={'MessageSender'}>
-        <table>
+        <table id="tabelaRegistros">
             <tbody>
                 <tr>
                     <td>
                         <div className="playercard" style={{ backgroundColor: "red" }} onClick={handleSubmit}>
+                            <p>{0}</p>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="playercard" style={{ backgroundColor: "green" }} onClick={handleSubmit}>
                             <p>{1}</p>
                         </div>
                     </td>
                     <td>
-                        <div className="playercard" style={{ backgroundColor: "green" }} onClick={handleSubmit}>
+                        <div className="playercard" style={{ backgroundColor: "blue" }} onClick={handleSubmit}>
                             <p>{2}</p>
                         </div>
                     </td>
                     <td>
-                        <div className="playercard" style={{ backgroundColor: "blue" }} onClick={handleSubmit}>
+                        <div className="playercard" style={{ backgroundColor: "yellow" }} onClick={handleSubmit}>
                             <p>{3}</p>
                         </div>
                     </td>
                     <td>
-                        <div className="playercard" style={{ backgroundColor: "yellow" }} onClick={handleSubmit}>
+                        <div className="playercard" style={{ backgroundColor: "red" }} onClick={handleSubmit}>
                             <p>{4}</p>
                         </div>
                     </td>
                     <td>
-                        <div className="playercard" style={{ backgroundColor: "red" }} onClick={handleSubmit}>
+                        <div className="playercard" style={{ backgroundColor: "green" }} onClick={handleSubmit}>
                             <p>{5}</p>
                         </div>
                     </td>
                     <td>
-                        <div className="playercard" style={{ backgroundColor: "green" }} onClick={handleSubmit}>
-                            <p>{6}</p>
-                        </div>
-                    </td>
-                    <td>
                         <div className="playercard" style={{ backgroundColor: "blue" }} onClick={handleSubmit}>
-                            <p>{7}</p>
+                            <p>{6}</p>
                         </div>
                     </td>
                 </tr>
                 {/* <tr>
                     <td>
-                        <div className="playercard" style={{ backgroundColor: "red" }} onClick={handleSubmit}>
+                        <div className="playercard" style={{ backgroundColor: "yellow" }} onClick={handleSubmit}>
                             <p>{7}</p>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="playercard" style={{ backgroundColor: "red" }} onClick={handleSubmit}>
+                            <p>{8}</p>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="playercard" style={{ backgroundColor: "green" }} onClick={handleSubmit}>
+                            <p>{9}</p>
                         </div>
                     </td>
                 </tr> */}
